9156d7cc8e2b1f23eaa43bc4f9c1a9dc67c18914
Francaosawaru <forierie@yahoo.com>
2023-11-02 04:32:47 -0700

    Everything can be imported

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,15 @@
-
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from variable_load_5 import a
+
+from variable_load_5 import a
+
+
+
+def find_var():
+
     print(a)
+
+
+
+if _name_ == "_main_":
+
+    find_var()
