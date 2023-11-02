6754f2b8a2bbac7d431b9f42f5e7e00132099cdb
Francaosawaru <forierie@yahoo.com>
2023-11-02 06:03:29 -0700

    updated

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@
 if _name_ == "_main_":
     """print all names defined by hidden_4 module."""
 import hidden_4
-# Print sorted name from directory
-for names =dir(hidden_4)
-if name[:2] != '__':
-print("{}".format(name))
+names = dir(hidden_4)
+for name in names:
+if name[:2] != "__":
+print(name)
