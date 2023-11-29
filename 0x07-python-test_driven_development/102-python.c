4378ee785665e4695ab7e8cc7750cd5a56f53ab2
Francaosawaru <forierie@yahoo.com>
2023-11-29 14:46:30 -0800

    done

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,29 +1,28 @@
-#!/usr/bin/python3
-"""defines function to scalar divde matrix"""
+#include "Python.h"
 
+/**
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
+ */
+void print_python_string(PyObject *p)
+{
+	long int length;
 
-def matrix_divided(matrix, div):
-    """divides matrix by scalar integer, rounded to two decimal places"""
-    import decimal
-    error_msg = "matrix must be a matrix (list of lists) of integers/floats"
-    if type(matrix) is not list:
-        raise TypeError(error_msg)
-    len_rows = []
-    row_count = 0
-    for row in matrix:
-        if type(row) is not list:
-            raise TypeError(error_msg)
-        len_rows.append(len(row))
-        for element in row:
-            if type(element) not in [int, float]:
-                raise TypeError(error_msg)
-        row_count += 1
-    if len(set(len_rows)) > 1:
-        raise TypeError("Each row of the matrix must have the same size")
-    if type(div) not in [int, float]:
-        raise TypeError("div must be a number")
-    if int(div) == 0:
-        raise ZeroDivisionError("division by zero")
-    new_matrix = list(map(lambda row:
-                          list(map(lambda x: round(x/div, 2), row)), matrix))
-    return new_matrix
+	fflush(stdout);
+
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	length = ((PyASCIIObject *)(p))->length;
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+}
