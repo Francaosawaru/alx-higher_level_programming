3524de4b76e2bc970de94250b3ab2806b6e65cce
Francaosawaru <forierie@yahoo.com>
2023-11-28 17:19:45 -0800

    done

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+    This module contains a locked class that
+    prevents the user from externally
+    creating new instance attributes except if
+    the attribute is "first_name"
+"""
