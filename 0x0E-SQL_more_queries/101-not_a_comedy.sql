bb15cb9241eebd9a072baf2b05ba3ba39e302a8b
Francaosawaru <forierie@yahoo.com>
2024-01-17 06:25:43 -0800

    updated

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,16 +1,12 @@
--- lists shows that don't belong to Comedy genre
-SELECT tv_shows.title
-[2;2R[>77;30604;0c]10;rgb:bfbf/bfbf/bfbf]11;rgb:0000/0000/0000FROM tv_shows
-LEFT JOIN
-(
-	SELECT tv_shows.title
-	FROM tv_shows
-     	JOIN tv_show_genres
-     	     ON tv_show_genres.show_id = tv_shows.id
-     	JOIN tv_genres
-     	     ON tv_genres.id = tv_show_genres.genre_id
-	WHERE tv_genres.name = "Comedy"
-	ORDER BY tv_shows.id
-) comedy_shows ON comedy_shows.title = tv_shows.title
-WHERE comedy_shows.title is NULL
-ORDER BY tv_shows.title;
+-- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+-- uses a database to list all rows not linked to one row
+SELECT title
+FROM tv_shows
+WHERE title NOT IN
+(SELECT title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy')
+GROUP BY title
+ORDER BY title ASC;
