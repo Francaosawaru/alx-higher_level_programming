2a1ce8119d13e8cbbff96b6355a1e2ab25e58dd4
francaosawaru <forierie@yahoo.com>
2024-03-01 17:47:57 +0000

    teat file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,7 +2,8 @@
 """Finds a peak in a list of unsorted integers"""
 
 def find_peak(list_of_integers):
-if len (list_of_integers) == 0:
+
+    if len (list_of_integers) == 0:
 return None
                 
 low = 0
