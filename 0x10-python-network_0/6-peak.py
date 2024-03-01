39f76bda6e61535e8e14ce46d04f54af8b0cc6b3
francaosawaru <forierie@yahoo.com>
2024-03-01 16:09:54 +0000

    find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,36 +1,24 @@
- #!/usr/bin/python3
- """
- Defines a peak-finding algorithm
- """
+#!/usr/bin/python3
+"""finds a peak in a list of unsorted integers"""
 
 
- def find_peak(list_of_integers):
-     """
-         Finds the highest number in a list of integers
+def find_peak(list_of_integers):
+        """Finds a peak in list_of_integers"""
 
-             Args:
-                         list_of_integers: List (int)
-                             Return:
-                                         the highest number
-                                             """
-
-                                                 if not list_of_integers:
-                                                         return None
-
-                                                             if len(list_of_integers) == 1:
-                                                                     return list_of_integers[0]
-                                                                         elif len(list_of_integers) == 2:
-                                                                                 return max(list_of_integers)
-                                                                                     else:
-                                                                                             left = 0
-                                                                                                     right = len(list_of_integers) - 1
-
-                                                                                                             while left < right:
-                                                                                                                         mid = (left + right) // 2
-
-                                                                                                                                     if list_of_integers[mid] < list_of_integers[mid + 1]:
-                                                                                                                                                     left = mid + 1
-                                                                                                                                                                 else:
-                                                                                                                                                                                 right = mid
-
-                                                                                                                                                                                         return list_of_integers[left]
+            if list_of_integers is None or list_of_integers == []:
+                    return None
+                        lo = 0
+                            hi = len(list_of_integers)
+                                mid = ((hi - lo) // 2) + lo
+                                    mid = int(mid)
+                                        if hi == 1:
+                                                return list_of_integers[0]
+                                                    if hi == 2:
+                                                            return max(list_of_integers)
+                                                                if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+                                                                                    list_of_integers[mid] >= list_of_integers[mid + 1]:
+                                                                                            return list_of_integers[mid]
+                                                                                                if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+                                                                                                        return find_peak(list_of_integers[mid:])
+                                                                                                            if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+                                                                                                                    return find_peak(list_of_integers[:mid])
