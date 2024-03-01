3f840e0a27267396d59ec5d0ae7e8a9d6fd3c487
francaosawaru <forierie@yahoo.com>
2024-03-01 16:32:44 +0000

    test file

--- 0x10-python-network_0/6-main.py
+++ 0x10-python-network_0/6-main.py
@@ -1,11 +1,24 @@
-ume@ubuntu:~/0x10$ cat 6-main.py
 #!/usr/bin/python3
-""" Test function find_peak """
-find_peak = __import__('6-peak').find_peak
+"""
+Defines a peak-finding algorithm
+"""
 
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
+ # Corrected import statement
+ from importlib import import_module
+
+ # Dynamic import of find_peak function from 6-peak.py
+ peak_module = import_module('6-peak')
+ find_peak = peak_module.find_peak
+
+ # Test function for find_peak
+ def test_find_peak():
+     """
+         Test function for find_peak
+             """
+                 peak = find_peak([1, 2, 4, 6, 3])
+                     print(peak)  # Example output: 6
+
+                     # Execute the test function
+                     if __name__ == "__main__":
+                         test_find_peak()
+
